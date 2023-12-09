af6638d9536182502db73657c0ef122d682de284
Acturest <2865257456@qq.com>
2023-12-09 21:29:56 +0800

    New UI

--- ui.py
+++ ui.py
@@ -1,6 +1,5 @@
-from tkinter import *
-from tkinter import ttk
 from tkinter import filedialog
+import ttkbootstrap as tkb
 from PIL import Image, ImageTk
 import threading as td
 import down
@@ -11,69 +10,88 @@ import os
 
 class VideoDownloader:
     def __init__(self, root):
+        self.style = tkb.Style()
         self.root = root
-        self.root.title('Down Video')
-        self.root.geometry('350x380+362+234')
-        self.root["bg"] = "white"
-        self.v, self.v0, self.folder_var, self.token = StringVar(), StringVar(), StringVar(), None
-        # 虚化 值越小虚化程度越高
-        # self.root.attributes('-alpha', 0.8)
-        # GUI components
+        self.v, self.v0, self.folder_var, self.token = tkb.StringVar(), tkb.StringVar(), tkb.StringVar(), None
+        self.flag = 1
         self.create_widgets()
 
     def create_widgets(self):
-        notebook = ttk.Notebook(self.root)
-        # 选项卡1
-        tab1 = ttk.Frame(notebook)
-        notebook.add(tab1, text='登录&验证')
-        tab1_top = ttk.Frame(tab1)
-        button_verify = ttk.Button(tab1_top, text="验证", width=10, command=self.accept_input, takefocus=False)
-        button_login = ttk.Button(tab1_top, text="登录", width=10, command=self.login_input, takefocus=False)
-        tab1_information = ttk.LabelFrame(tab1, text='二维码', width=200, height=200, borderwidth=2, relief="sunken")
+        # 主框架
+        tab1 = tkb.Frame(self.root, width=1100, height=625)
+        tab1.grid_propagate(False)
+        tab1.pack()
+        # 子框架1
+        down_top_frame = tkb.Frame(tab1, width=875, height=625)
+        down_top_frame.grid_propagate(False)
+        down_top_frame.grid(row=0, column=0)
+        title_frame = tkb.Frame(down_top_frame, width=800, height=80)
+        title_frame.pack_propagate(False)
+        title_frame.grid(row=0, pady=20)
+        title = tkb.Label(title_frame, text='BiliBili Video Download', font=('Arial', 24, "bold"), foreground='Sky Blue')
+        dividing_line1 = tkb.Separator(title_frame, bootstyle="primary")
+        title.pack(anchor='w'), dividing_line1.pack(fill='x', pady=10)
+
+        foundation_frame = tkb.LabelFrame(down_top_frame, text='基础', width=835, height=125, bootstyle="info")
+        foundation_frame.grid_propagate(False)
+        foundation_frame.grid(row=1, padx=20)
+        file_save = tkb.Label(foundation_frame, text="保存地址: ", bootstyle="primary")
+        self.folder_var.set(open(r'.\tmp\video_file.txt', 'r').read())
+        self.entry2 = tkb.Entry(foundation_frame, width=55, textvariable=self.folder_var, state="readonly", bootstyle="primary")
+        file_save.grid(row=0, column=0, padx=10, pady=5), self.entry2.grid(row=0, column=1, pady=5)
+        down_options = tkb.Label(foundation_frame, text="下载选项: ", bootstyle="primary")
+        self.combobox2 = tkb.Combobox(foundation_frame, values=["视频+音频", "仅音频"], state="readonly", bootstyle="primary")
+        self.combobox2.current(0)
+        down_options.grid(row=1, column=0, padx=10, pady=10), self.combobox2.grid(row=1, column=1, sticky='nsew', pady=10)
+        save_button = tkb.Button(foundation_frame, text="-->", command=self.save_input, takefocus=False, width=5)
+        open_button = tkb.Button(foundation_frame, text="open", command=self.open_input, takefocus=False, width=5)
+        save_button.grid(row=0, column=2, padx=20), open_button.grid(row=0, column=3, padx=20)
+
+        down_frame = tkb.LabelFrame(down_top_frame, text='下载', width=835, height=360, bootstyle="info")
+        down_frame.pack_propagate(False)
+        down_frame.grid(row=2, padx=20)
+
+        url_frame = tkb.Frame(down_frame, width=835, height=120)
+        url_frame.grid_propagate(False)
+        url_frame.pack()
+        down_url = tkb.Label(url_frame, text="视频地址: ", bootstyle="primary")
+        self.entry = tkb.Entry(url_frame, width=60, bootstyle="primary")
+        down_button = tkb.Button(url_frame, text="Down", width=10, command=self.process_input, takefocus=False)
+        down_url.grid(row=0, column=0, padx=10), self.entry.grid(row=0, column=1), down_button.grid(row=0, column=2, padx=15)
+        down_video_options = tkb.Label(url_frame, text="视频清晰度:", bootstyle="primary")
+        self.combobox1 = tkb.Combobox(url_frame, values=["1080P", "720P", "360P"], state="readonly", bootstyle="primary")
+        self.combobox1.current(0)
+        down_video_options.grid(row=1, column=0, padx=5, pady=5), self.combobox1.grid(row=1, column=1, pady=5, sticky='nsew')
+        self.mouse()  # 禁止鼠标滚轮控制选项
+
+        log_frame = tkb.LabelFrame(down_frame, text='log', width=800, height=200, bootstyle="info")
+        log_frame.pack_propagate(False)
+        video_cover = tkb.Label(log_frame, text="视频标题: ????", bootstyle="primary")
+        log_frame.pack(), video_cover.pack(padx=10, anchor='w')
+        time_progress = tkb.Frame(log_frame, width=750, height=50)
+        time_progress.pack_propagate(False)
+        self.log_text = tkb.Label(time_progress, width=100, textvariable=self.v0, font=('黑体', 10), foreground='green')
+        self.v0.set("下载状态")
+        time_progress.pack(side='bottom', pady=10), self.log_text.pack(anchor='w')
+        # 进度条
+        self.progress = tkb.Progressbar(time_progress)
+
+        # 子框架2
+        tab1_top = tkb.Frame(tab1, width=225, height=635, relief='ridge')
+        tab1_top.pack_propagate(False)
+        tab1_top.grid(row=0, column=1)
+        log_1 = tkb.Label(tab1_top, background="blue", width=60, textvariable=self.v, anchor='center', foreground='white')
+        self.v.set('信息显示')
+        button_verify = tkb.Button(tab1_top, text="验证", width=10, command=self.accept_input, takefocus=False)
+        button_login = tkb.Button(tab1_top, text="登录", width=10, command=self.login_input, takefocus=False)
+        log_1.pack(fill="x", side='top'), button_login.pack(pady=20), button_verify.pack()
+        tab1_information = tkb.LabelFrame(tab1_top, text='img', width=200, height=200, borderwidth=2, relief="sunken")
         image = Image.open(r'.\tmp\0035.png')
         self.photo = ImageTk.PhotoImage(image)
-        self.label = ttk.Label(tab1_information, image=self.photo)
-        image_button = ttk.Button(tab1_information, text="清除缓存", command=self.login_accept, takefocus=False)
-        log_1 = ttk.Label(tab1, background="yellow", width=60, textvariable=self.v, anchor='center')
-        self.v.set('验证以及登录的信息将显示在这里')
-        tab1_top.pack(pady=10), button_login.grid(row=0, column=0, padx=10), button_verify.grid(row=0, column=1, padx=10)
-        tab1_information.pack(), self.label.pack(), image_button.pack()
-        log_1.pack(fill="both", side='bottom')
-        # 选项卡2
-        tab2 = ttk.Frame(notebook)
-        notebook.add(tab2, text='下载视频')
-        text1 = ttk.Label(tab2, text="视频地址(支持av号):")
-        self.entry = ttk.Entry(tab2, width=60)
-        text2 = ttk.Label(tab2, text="视频清晰度:")
-        options1 = ["1080P", "720P", "360P"]
-        self.combobox1 = ttk.Combobox(tab2, values=options1, state="readonly")
-        self.combobox1.current(0)
-        text3 = ttk.Label(tab2, text="下载选项:")
-        options2 = ["视频+音频", "仅音频"]
-        self.combobox2 = ttk.Combobox(tab2, values=options2, state="readonly")
-        self.combobox2.current(0)
-        text4 = ttk.Label(tab2, text="文件保存地址:")
-        self.folder_var.set(open(r'.\tmp\video_file.txt', 'r').read())
-        self.entry2 = ttk.Entry(tab2, width=80, textvariable=self.folder_var, state="readonly")
-        file_button = ttk.Frame(tab2)
-        save_button = ttk.Button(file_button, text="选择保存位置", command=self.save_input, takefocus=False)
-        open_button = ttk.Button(file_button, text="打开文件地址", command=self.open_input, takefocus=False)
-        # 禁止鼠标滚轮控制选项
-        self.mouse()
-        down_button = ttk.Button(tab2, text="下载", width=10, command=self.process_input, takefocus=False)
-        log_2 = ttk.Label(tab2, background="yellow", width=100, textvariable=self.v0, anchor='center')
-        self.v0.set("这里将显示部分错误信息以及下载状态")
-        # 进度条
-        self.progress = ttk.Progressbar(tab2)
-        # 放置主窗口
-        text1.pack(side="top", anchor='nw'), self.entry.pack(fill="both")
-        text2.pack(side="top", anchor='w'), self.combobox1.pack(side="top", anchor='w')
-        text3.pack(side="top", anchor='w'), self.combobox2.pack(side="top", anchor='w')
-        text4.pack(side="top", anchor='w'), self.entry2.pack(side="top", anchor='w'),
-        file_button.pack(), save_button.grid(row=0, column=0, padx=40), open_button.grid(row=0, column=1, padx=40)
-        down_button.pack(padx=5, pady=5)
-        log_2.pack(fill="both", side='bottom')
-        notebook.pack(side="top", fill="both", expand=True)
+        self.label = tkb.Label(tab1_information, image=self.photo)
+        image_button = tkb.Button(tab1_top, text="清除缓存", command=self.login_accept, takefocus=False)
+        topic = tkb.Checkbutton(tab1_top, text='dark', bootstyle="round-toggle", command=self.theme)
+        tab1_information.pack(pady=30), self.label.pack(), image_button.pack(), topic.pack(side="bottom", pady=25)
 
     def process_input(self):
         user_input = self.entry.get()
@@ -102,7 +120,7 @@ class VideoDownloader:
         length_block = int(length * 0.02) + 10000
         # 进度条
         self.progress['maximum'], self.progress['value'] = length, 0
-        self.progress.pack(pady=10)
+        self.progress.pack(side='bottom', fill='x', pady=5)
         with open(self.entry2.get() + r'\{}'.format(file_suffix), 'wb') as f:
             # 获取下载进度
             start_time = time.time()
@@ -114,6 +132,27 @@ class VideoDownloader:
         end_time = time.time()
         self.v0.set("下载完成,总用时: {:d}s".format(int(end_time - start_time)))
 
+    def mouse(self):
+        def disable_scroll(event):
+            return "break"
+
+        self.combobox1.bind("<MouseWheel>", disable_scroll)
+        self.combobox2.bind("<MouseWheel>", disable_scroll)
+
+    #
+    def save_input(self):
+        file_path = filedialog.askdirectory()
+        if file_path:
+            self.folder_var.set(file_path)
+            open(r'.\tmp\video_file.txt', "w").write(file_path)
+
+    def open_input(self):
+        folder_path = os.path.abspath(self.entry2.get())
+        if os.path.exists(folder_path):
+            os.startfile(folder_path)
+        else:
+            self.v0.set("所选保存路径不存在")
+
     def image_processing(self, image_file):
         image = Image.open(image_file)
         self.photo = ImageTk.PhotoImage(image)
@@ -124,6 +163,14 @@ class VideoDownloader:
             self.v.set(verify.accept())
         else:
             self.v.set("未登录,请先登录")
+
+    def theme(self):
+        if self.flag == 1:
+            self.style.theme_use('darkly')
+            self.flag = 0
+        else:
+            self.style.theme_use('cosmo')
+            self.flag = 1
 
     def thread_processing(self):
         self.v.set('已登录')
@@ -142,7 +189,7 @@ class VideoDownloader:
     def login_input(self):
         self.token = verify.login()
         self.image_processing(r'.\tmp\my_blog.png')
-        t1 = td.Thread(target=self.qrcode_thread).start()
+        td.Thread(target=self.qrcode_thread).start()
 
     def login_accept(self):
         if os.path.isfile(r'.\tmp\my_blog.png'):
@@ -151,28 +198,8 @@ class VideoDownloader:
             os.remove(r'.\tmp\your_cookie.txt')
         self.v.set('已清除缓存')
 
-    def mouse(self):
-        def disable_scroll(event):
-            return "break"
-
-        self.combobox1.bind("<MouseWheel>", disable_scroll)
-        self.combobox2.bind("<MouseWheel>", disable_scroll)
-
-    def save_input(self):
-        file_path = filedialog.askdirectory()
-        if file_path:
-            self.folder_var.set(file_path)
-            open(r'.\tmp\video_file.txt', "w").write(file_path)
-
-    def open_input(self):
-        folder_path = os.path.abspath(self.entry2.get())
-        if os.path.exists(folder_path):
-            os.startfile(folder_path)
-        else:
-            self.v0.set("所选保存路径不存在")
-
 
 if __name__ == "__main__":
-    root = Tk()
+    root = tkb.Window(title='Download UI', themename='cosmo', size=(1100, 625), position=(400, 250), resizable=None)
     app = VideoDownloader(root)
     root.mainloop()
